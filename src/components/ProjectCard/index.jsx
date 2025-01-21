442b15ed45ef98b8e14605de5765223e326916e0
Lucas Gervasoni <jaxellucas@gmail.com>
2025-01-21 18:40:07 -0300

    changing project card style

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,9 +1,8 @@
-'use client'
-import estilos from './ProjectCard.module.css';
-import { useState } from 'react'
+"use client";
+import estilos from "./ProjectCard.module.css";
+import { useState } from "react";
 
 export default function ProjectCard({ projects }) {
-
   const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
 
   const handleNext = () => {
@@ -11,43 +10,34 @@ export default function ProjectCard({ projects }) {
   };
 
   const handlePrev = () => {
-    setCurrentProjectIndex((prevIndex) => (prevIndex - 1 + projects.length) % projects.length);
+    setCurrentProjectIndex(
+      (prevIndex) => (prevIndex - 1 + projects.length) % projects.length
+    );
   };
 
   return (
     <div id={estilos.Card}>
-    {projects.map((projeto, index) => (
-      index === currentProjectIndex && (
-      <div key={index} className={estilos.card}>
-        <div className={estilos.hero_projects}>
-        <span onClick={handlePrev} className={estilos.prev}><img src="./util/prev.svg" alt="" /></span>
-          <figure>
-            <img style={{ width: '700px', height: '500px'}} src={projeto.project_image} alt="" />
-          </figure>
-          <span onClick={handleNext} className={estilos.next}><img src="./util/next.svg" alt="" /></span>
+      {projects.map((projeto, index) => (
+        <div key={index} className={estilos.card}>
+          <div className={estilos.hero_projects}>
+            <figure className={estilos.project_img}>
+              <img src={projeto.project_image} alt={projeto.name} />
+            </figure>
+            <div className={estilos.hero_info}>
+              <h3 className={estilos.project_name}>{projeto.name}</h3>
+              <p className={estilos.project_desc}>{projeto.desc}</p>
+              <a
+                className={estilos.project_link}
+                href={projeto.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                SOURCE CODE
+              </a>
+            </div>
+          </div>
         </div>
-
-        <div className={estilos.hero_info}>
-          <h3 className={estilos.project_name}>{projeto.name}</h3>
-          <ul className={estilos.project_skills}>
-          {projeto.tecnologias.map((tech, idx) => (
-                <li className={estilos.project_skill} key={idx}>
-                  <img
-                    style={{ width: '24px', height: '24px' }}
-                    src={tech.icone}
-                    alt={tech.nome}
-                    title={tech.nome}
-                  />
-                  {/* <p className={estilos.skill_name}>{tech.nome}</p> */}
-                </li>
-              ))}
-          </ul>
-          <p className={estilos.project_desc}>{projeto.desc}
-          </p>
-          <a className={estilos.project_link} href={projeto.link} target='_blank'>SOURCE CODE</a>
-        </div>
-      </div>
-    )))}
+      ))}
     </div>
   );
 }
